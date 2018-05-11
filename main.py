2693acfa13e0054ec2906eeb1c8fdaddfebea012
alex-ht <hung_alex@icloud.com>
2018-05-11 21:53:32 +0800

    bug

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 import os
 import RssNewsParser
+from flask import Flask
 
-if __name__ == '__main__':
+
+app = Flask(__name__)
+@app.route('/')
+def parse_news():
     RssNewsParser.read_file('news.db', '/tmp/news.db')
     # 建立資料庫
     conn = RssNewsParser.open_or_create_db('/tmp/news.db')
@@ -13,3 +17,11 @@ if __name__ == '__main__':
     with open('/tmp/news.db', 'rb') as fin:
         RssNewsParser.upload_file(fin.read(), 'news.db', 'application/x-sqlite3')
     os.remove('/tmp/news.db')
+
+
+
+@app.errorhandler(500)
+def server_error(e):
+    # Log the error and stacktrace.
+    logging.exception('An error occurred during a request.')
+    return 'An internal error occurred.', 500
