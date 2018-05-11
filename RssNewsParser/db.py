0b9e517caff1fad4278ee4221c0772113539b210
alex-ht <hung_alex@icloud.com>
2018-05-11 18:52:53 +0800

    bug

--- RssNewsParser/db.py
+++ RssNewsParser/db.py
@@ -7,7 +7,7 @@ def open_or_create_db(path):
     else:
         conn = sqlite3.connect(path)
         c = conn.cursor()
-        c.execute('CREATE TABLE news(key TEXT PRIMARY KEY, source TEXT, date TEXT, content TEXT, wave TEXT, youtube_key INTEGER UNIQUE)')
+        c.execute('CREATE TABLE news(key TEXT PRIMARY KEY, source TEXT, date TEXT, content TEXT, wave TEXT, youtube_key TEXT UNIQUE)')
         conn.commit()
         return conn;
 
@@ -17,4 +17,4 @@ def close_db(conn):
 
 def insert_record(conn, key, source, date, content, wave, youtube_key):
     c = conn.cursor()
-    c.execute('INSERT INTO news VALUES (%s, %s, %s, %s, %s, %d)' % (key, source, date, content, wave, int(youtube_key)))
+    c.execute('INSERT INTO news VALUES (\'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\')' % (key, source, date, content, wave, youtube_key))
