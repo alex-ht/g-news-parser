15cbd9e975063abf9c6c863795a585a847d2208a
Alex Hung <hung_alex@icloud.com>
2018-05-12 16:53:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -27,3 +27,8 @@ def server_error(e):
     # Log the error and stacktrace.
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
